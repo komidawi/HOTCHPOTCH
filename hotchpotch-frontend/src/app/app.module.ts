e37984cd55d1df11f962b89b1d3c38f633773840
Dawid Komisarczuk <komidawi@gmail.com>
2024-12-18 19:36:23 +0100

    Feat: Add PizzaCalculatorComponent with basic setup
    Introduced a new PizzaCalculatorComponent, including its HTML template, CSS, and unit test setup. Integrated the component into the AppModule to make it available for use. Initial functionality includes a placeholder template and a passing test case verifying the component's creation.

--- hotchpotch-frontend/src/app/app.module.ts
+++ hotchpotch-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PizzaCalculatorComponent } from './pizza-calculator/pizza-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaCalculatorComponent
   ],
   imports: [
     BrowserModule
---
+++ hotchpotch-frontend/src/app/pizza-calculator/pizza-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-calculator',
+  standalone: false,
+
+  templateUrl: './pizza-calculator.component.html',
+  styleUrl: './pizza-calculator.component.css'
+})
+export class PizzaCalculatorComponent {
+
+}
